a13dc704d12217877640cd4d9292079d75257d5c
Jack Wong <jack7a5a1993@icloud.com>
2018-06-18 10:43:57 +0900

    AR jelly fish

---
+++ AR_JellyFish/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-AR_JellyFish.build/Objects-normal/x86_64/Pods_AR_JellyFish_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jackwong/Desktop/Developer/AR_JellyFish/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-AR_JellyFish.build/DerivedSources/Pods_AR_JellyFish_vers.c
