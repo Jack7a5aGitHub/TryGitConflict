de83f212cbf08a535f82b5683c8c76b141ca36be
Jack Wong <jack7a5a1993@icloud.com>
2018-06-18 11:20:52 +0900

    swiftlint

--- AR_JellyFish/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-AR_JellyFish.build/Objects-normal/arm64/Pods_AR_JellyFish_vers.d
+++ AR_JellyFish/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-AR_JellyFish.build/Objects-normal/arm64/Pods_AR_JellyFish_vers.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /Users/jackwong/Desktop/Developer/AR_JellyFish/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-AR_JellyFish.build/DerivedSources/Pods_AR_JellyFish_vers.c
+  /Users/jackwong/TryGitConflict/AR_JellyFish/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-AR_JellyFish.build/DerivedSources/Pods_AR_JellyFish_vers.c
